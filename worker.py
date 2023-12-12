1aebe74248252fc830beb6b8ef8a3183fbe12dbd
jordanx2 <jordanmooney9@gmail.com>
2023-12-12 23:07:54 +0000

    fix

--- worker.py
+++ worker.py
@@ -13,7 +13,7 @@ def get_api_key() -> str:
         return secret
 
     # If not found in environment variables, fetch from Secret Manager
-    project_id = "cloudcomputelab6"  # Replace with your project ID
+    project_id = "334590066640"  # Replace with your project ID
     secret_id = "compute-api-key"  # The secret name in Secret Manager
 
     client = secretmanager.SecretManagerServiceClient()
